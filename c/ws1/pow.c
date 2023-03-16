c7c3f153123ad3a2018007f56575858e34b50dbb
Bar.Shadkhin <b.shadkhin@gmail.com>
2023-03-16 18:58:15 +0200

    Exe.4

---
+++ c/ws1/pow.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+double Pow(int n);
+
+
+void main()
+{
+	printf("%f\n",Pow(-5));	
+}
+
+
+double Pow(int n)
+{
+	double result = 1, base = 10; 
+
+	if(n < 0)
+	{
+		n=-n;
+		base = 0.1;
+	}
+
+	for(int i = 0;i < n; i++)
+	{
+		result *= base ;
+	}
+	
+		return result;
+}

b7a88a169eeed346992c2657e51370933b9a9428
Bar.Shadkhin <b.shadkhin@gmail.com>
2023-03-19 13:01:39 +0200

    CpArr.c v.2

--- c/ws2/CpArr.c
+++ c/ws2/CpArr.c
@@ -11,21 +11,23 @@ void main()
 	char *p = Original;
 	char *Copy = CpArr(p, 3);
 	printf("%s\n", Copy);
+	free(Copy);
+	Copy == NULL; 
 	
 }
 
 
-char* CpArr(char *ArrToCopy, int SIZE) //The function returns a char type pointer and receives 
-				       //a pointer to a char array and its size				
+char* CpArr(char *ArrToCopy, int SIZE) /*The function returns a char type pointer and receives*/ 
+				       /*a pointer to a char array and its size*/				
 {	 
-	char *p = (char*)malloc(SIZE * sizeof(int)); //Allocating the memory of the array 
-						    // to the pointer
+	char *p = (char*)malloc(SIZE * sizeof(int)); /*Allocating the memory of the array */
+						    /*to the pointer*/
 	assert(ArrToCopy != 0);					 
 						 
-	int i; //Defining the index for the loop
+	int i; /*Defining the index for the loop*/
 	
 
-	for(i = 0; i < SIZE; i++) //Looping over the array and assinging the address to the pointer
+	for(i = 0; i < SIZE; i++) /*Looping over the array and assinging the address to the pointer*/
 	{
 		p[i] = ArrToCopy[i]; 
 		

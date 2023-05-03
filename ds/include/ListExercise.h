53bd913c3c1e7c3354931a7bff9a896a24789ef7
Bar <b.shadkhin@gmail.com>
2023-05-03 08:30:37 +0300

    List Exercised - Approved, MariaP

--- ds/include/ListExercise.h
+++ ds/include/ListExercise.h
@@ -20,33 +20,40 @@ typedef struct node
 
 /**************************************************************/
 /*
-Description: 
+Description: Flips the given list.
 Arguments: 
+	- head : A valid pointer to the head of the list.
 Return: 
-Time complexity: 
-Space complexity: 
+	Returns the head to the fliped list.
+Time complexity: O(1)
+Space complexity: O(1)
 */
 /*Reverses the order of a given Slist.*/
 node_t *Flip(node_t *head);
 
 /**************************************************************/
 /*
-Description: 
+Description: Check whether or not the list is looped.
 Arguments: 
+	- head : A valid pointer to the head of the list.
 Return: 
-Time complexity: 
-Space complexity: 
+	1 if looped, 0 otherwise. 
+Time complexity: O(n)
+Space complexity: O(1)
 */
 /*Tells whether a given slist has loop*/
 int HasLoop(const node_t *head);
 
 /**************************************************************/
 /*
-Description: 
-Arguments: 
+Description: Check if there is an intersection beteween lists, returns the intersection point if so. 
+Arguments:
+	- head1: A valid pointer to the head of the list.
+	- head2: A valid pointer to the head of the list. 
 Return: 
-Time complexity: 
-Space complexity: 
+	The intersection point, NULL otherwise.
+Time complexity: O(n)
+Space complexity: O(1) 
 */
 /*Returns a pointer to a first node mutual to both slist, if any */
 node_t *FindIntersection(node_t *head1, node_t *head2);

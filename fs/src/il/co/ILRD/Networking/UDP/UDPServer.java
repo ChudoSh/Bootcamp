dee99aec5c0bb6bfa8e3bef7f8250ede5a376904
Bar <b.shadkhin@gmail.com>
2023-09-13 14:24:09 +0300

    UDP, test impl.

--- fs/src/il/co/ILRD/Networking/UDP/UDPServer.java
+++ fs/src/il/co/ILRD/Networking/UDP/UDPServer.java
@@ -5,59 +5,59 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
-import java.net.InetAddress;
-import java.nio.charset.StandardCharsets;
 
 public class UDPServer {
-    private DatagramSocket datagramSocket;
-    private int port;
+    public static void main(String[] args) throws IOException {
+        new UDPServer(12345);
+    }
+
+    private final DatagramSocket datagramSocket;
 
     public UDPServer(int port) throws IOException {
         this.datagramSocket = new DatagramSocket(port);
-        this.port = port;
-
 
         try {
             System.out.println("DatagramSocket Started : Server");
             System.out.println(this.datagramSocket);
             System.out.println("CLIENT CONNECTED");
             chatServer();
-            datagramSocket.close();
         } catch (Exception e) {
             System.out.println(e);
+        } finally {
+            datagramSocket.close();
         }
+
     }
 
-    private void chatServer() throws IOException {
+    public void chatServer() {
         DatagramPacket sendPacket;
         DatagramPacket receivePacket;
         byte[] buffer;
-        String str = null;
+        String str;
 
         while (true) {
-            System.out.println("Please insert message:");
-            BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-            str = reader.readLine();
-
-            if (str.equals("bye")) {
-                break;
-            }
-
-            buffer = new byte[100];
+            buffer = new byte[1024];
             receivePacket = new DatagramPacket(buffer, buffer.length);
-            datagramSocket.receive(receivePacket);
-            str = new String(buffer, 0, receivePacket.getLength());
-            System.out.println("Message from Client:" + str);
+            try {
+                datagramSocket.receive(receivePacket);
+                str = new String(buffer, 0, receivePacket.getLength());
+                System.out.println("Message from Client:" + str);
 
-            buffer = str.getBytes();
-            sendPacket = new DatagramPacket(buffer, buffer.length, receivePacket.getAddress(), receivePacket.getPort());
-            datagramSocket.send(sendPacket);
+                System.out.println("Please insert message:");
+                BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+                str = reader.readLine();
+
+                if (str.equals("bye")) {
+                    break;
+                }
+
+                buffer = str.getBytes();
+                sendPacket = new DatagramPacket(buffer, buffer.length, receivePacket.getAddress(), receivePacket.getPort());
+                datagramSocket.send(sendPacket);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
             System.out.println("Message sent");
-
         }
-    }
-
-    public static void main(String[] args) throws IOException {
-        new UDPServer(1234);
     }
 }
